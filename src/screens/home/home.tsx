50567128d53d039edc800203e8e690e060ef030f
Warllei Martins <warllei.s.martins@hotmail.com>
2023-06-15 12:07:45 -0300

    mais conteúdo de estudo

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -15,10 +15,16 @@ import { CustomComponent } from '../../../src/styled-components/styled';
 export default function Home() {
 
     const [show, setShow] = useState(false)
+    const [showPassword, setShowPassword] = useState(true)
+    const [selectedIcon, setSelectedIcon] = useState('home');
 
     const handlerShow = () => {
         setShow(!show)
     }
+
+    const handlePress = (iconName: any) => {
+        setSelectedIcon(iconName);
+    };
 
     return (
         <GestureHandlerRootView style={{ flex: 1, justifyContent: 'space-between' }}>
@@ -42,7 +48,25 @@ export default function Home() {
                     </View>
 
                     <View style={{ display: 'flex', flexDirection: "column", marginTop: 50 }}>
-                        <Text style={{ fontSize: 40, color: "#3f3e3e", fontWeight: "600" }}>R$5,764,98</Text>
+                        <View style={{ display: 'flex', flexDirection: "row", justifyContent: 'space-between' }}>
+                            {
+                                showPassword
+                                    ?
+                                    <>
+                                        <Text style={{ fontSize: 40, color: "#3f3e3e", fontWeight: "600" }}>R$5,764,98</Text>
+                                        <TouchableOpacity onPress={() => setShowPassword(false)}>
+                                            <Bell name="eye" size={30} color={"#4e1762"} />
+                                        </TouchableOpacity>
+                                    </>
+                                    :
+                                    <>
+                                        <Text style={{ fontSize: 40, color: "#3f3e3e", fontWeight: "600" }}>•••••••</Text>
+                                        <TouchableOpacity onPress={() => setShowPassword(true)}>
+                                            <Bell name="eye-off" size={30} color={"#4e1762"} />
+                                        </TouchableOpacity>
+                                    </>
+                            }
+                        </View>
                         <View style={{ display: 'flex', flexDirection: "row", justifyContent: 'space-between' }}>
                             <Text style={{ fontSize: 15, color: "#000" }}>Seu Balanço</Text>
                             <Text style={{ fontSize: 15, color: "#ccc" }}>Últimos 5 Dias</Text>
@@ -70,7 +94,7 @@ export default function Home() {
                 <View style={{ marginTop: 48 }}>
                     <Text style={{ fontWeight: "600" }}>Transferência Rápida</Text>
                     <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{ marginTop: 20 }}>
-                        <TouchableOpacity style={{ marginRight: 40, height: 38, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        <TouchableOpacity style={{ marginRight: 40, height: 35, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                             <Bell name="user-plus" size={25} color={"#550683"} />
                         </TouchableOpacity>
                         <TouchableOpacity style={{ display: 'flex', flexDirection: "row", alignItems: 'center', marginRight: 25 }}>
@@ -135,26 +159,22 @@ export default function Home() {
             <BottomSheet show={show} handlerShowContent={setShow} />
 
             <View style={styles.containerNav}>
-                <TouchableOpacity style={{ padding: 20, borderRadius: 100 }}
-                >
-                    <HomePage name="home" size={25} color={"#560585"} />
+                <TouchableOpacity style={{ padding: 20, borderRadius: 100 }} onPress={() => handlePress('home')}>
+                    <HomePage name="home" size={25} color={selectedIcon === 'home' ? '#560585' : '#56058582'} />
                 </TouchableOpacity>
-                <TouchableOpacity style={{ padding: 20, borderRadius: 100 }}
-                >
-                    <HomePage name="wallet" size={25} color={"#560585"} />
+                <TouchableOpacity style={{ padding: 20, borderRadius: 100 }} onPress={() => handlePress('wallet')}>
+                    <HomePage name="wallet" size={25} color={selectedIcon === 'wallet' ? '#560585' : '#56058582'} />
                 </TouchableOpacity>
                 <View style={{ top: -60, padding: 20, backgroundColor: "#560585", borderRadius: 100 }}>
                     <TouchableOpacity onPress={handlerShow} >
                         <Icon name="arrow-swap" size={25} color={"#fff"} />
                     </TouchableOpacity>
                 </View>
-                <TouchableOpacity style={{ padding: 20, borderRadius: 100 }}
-                >
-                    <HomePage name="user" size={25} color={"#560585"} />
+                <TouchableOpacity style={{ padding: 20, borderRadius: 100 }} onPress={() => handlePress('user')}>
+                    <HomePage name="user" size={25} color={selectedIcon === 'user' ? '#560585' : '#56058582'} />
                 </TouchableOpacity>
-                <TouchableOpacity style={{ padding: 20, borderRadius: 100 }}
-                >
-                    <Entypo name="line-graph" size={25} color={"#560585"} />
+                <TouchableOpacity style={{ padding: 20, borderRadius: 100 }} onPress={() => handlePress('graph')}>
+                    <Entypo name="line-graph" size={25} color={selectedIcon === 'graph' ? '#560585' : '#56058582'} />
                 </TouchableOpacity>
             </View>
 
